8cda8bd9e7328fdb132e6ebc2370c5af34c5188a
mprambadi <mprambadi@gmail.com>
2017-11-01 20:38:00 +0700

    cleaning some unused libraries

--- layout/footer.php
+++ layout/footer.php
@@ -10,18 +10,7 @@
 <script src="../assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Select2 -->
 <script src="../assets/bower_components/select2/dist/js/select2.full.min.js"></script>
-<!-- Morris.js charts -->
-<script src="../assets/bower_components/raphael/raphael.min.js"></script>
-<!-- <script src="../assets/bower_components/morris.js/morris.min.js"></script> -->
-<!-- Sparkline -->
-<script src="../assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="../assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="../assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="../assets/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="../assets/bower_components/moment/min/moment.min.js"></script>
 <script src="../assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
 <!-- datepicker -->
 <script src="../assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
@@ -30,16 +19,13 @@
 <!-- DataTables -->
 <script src="../assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="../assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<!-- Slimscroll -->
-<script src="../assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="../assets/bower_components/fastclick/lib/fastclick.js"></script>
+
 <!-- AdminLTE App -->
 <script src="../assets/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!-- <script src="../assets/dist/js/pages/dashboard.js"></script> -->
 <!-- AdminLTE for demo purposes -->
-<script src="../assets/dist/js/demo.js"></script>
+<!-- <script src="../assets/dist/js/demo.js"></script> -->
 
 <script>
 
