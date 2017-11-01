4a77330f364145af88cc8b683ca0ccf822a4d0f0
mprambadi <mprambadi@gmail.com>
2017-11-01 20:44:22 +0700

    refactoring connection

--- db.php
+++ db.php
@@ -1,10 +1,14 @@
 <?php
 //Establishing Connection with Server by passing server_name, user_id and password as a parameter 
-$mysqli = new mysqli("localhost", "root", "cobacoba", "dispose");
+$host= '';
+$username= '';
+$database= '';
+$password= '';
+
+$mysqli = new mysqli($host, $username, $password, $database);
 
 if ($mysqli->connect_errno) {
   echo  "Failed to connect to MySQL: (" .$mysqli->connect_errno .")" .$mysqli->connect_error;
 }
 
-//Selecting Database
 ?>
